c3c1cbe9bdfcb453bce1f6ca29185be704804ac7
Ricardo Nenê <ricardonene@gmail.com>
2013-06-21 20:38:58 -0300

    Criacao do id

--- ProjetoTeste/src/Teste.java
+++ ProjetoTeste/src/Teste.java
@@ -8,5 +8,14 @@
  * @author ricardo.nene
  */
 public class Teste {
-    
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+  
 }
